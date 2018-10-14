4c87ae8b68b1e5787ea17291804a81b2134868ac
ziv.lin <730470317@qq.com>
2018-10-14 15:33:55 +0800

    [Add] add configuration of cmder

---
+++ Windows/cmder/cmder/config/user-aliases.cmd
@@ -0,0 +1,18 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+gst=git status
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+tf_board_page=explorer "http://PC-ziv-lin:6006" 
+tf_board=explorer "http://PC-ziv-lin:6006" && tensorboard.exe --logdir="./"
