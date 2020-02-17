7999eb4c7a3ac9b430b04a27282ab3c214dd9665
ziv.lin <ziv.lin.ljr@gmail.com>
2020-02-17 22:36:29 +0800

    [enh] add some tips, fix vscode can not fold region

--- common/set_4K_scale.sh
+++ common/set_4K_scale.sh
@@ -1,1 +1,3 @@
+# if this function take no effect, try read the display ID via:
+# dconf read /com/ubuntu/user-interface/scale-factor 
 dconf write /com/ubuntu/user-interface/scale-factor "{'DP-0':12}"
